a384909ebbea0c4e9bdf465e79870845dca3c6d9
Brendan W. McAdams <brendan@10gen.com>
2010-11-15 21:32:46 -0500

    BSONWritable work: Fixed write method (Forgot to call done) and added toString method

--- src/main/com/mongodb/hadoop/io/BSONWritable.java
+++ src/main/com/mongodb/hadoop/io/BSONWritable.java
@@ -1,18 +1,18 @@
 // BSONWritable.java
-/**
- *      Copyright (C) 2008 10gen Inc.
- *  
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+/*
+ * Copyright 2010 10gen Inc.
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.mongodb.hadoop.io;
@@ -23,11 +23,14 @@ import java.util.*;
 import org.bson.*;
 import org.bson.io.*;
 
+import org.apache.commons.logging.*;
+
 import org.apache.hadoop.io.*;
 
-@SuppressWarnings("deprecation")
+@SuppressWarnings( "deprecation" )
 public class BSONWritable implements BSONObject, Writable {
 
+    private static final Log log = LogFactory.getLog( BSONWritable.class );
 
     /**
      * Constructs a new instance.
@@ -40,110 +43,111 @@ public class BSONWritable implements BSONObject, Writable {
      * Constructs a new instance around an existing BSONObject
      */
     public BSONWritable(BSONObject doc) {
-        _doc = doc; // TODO - should we deepclone this?
+        this();
+        putAll( doc );
     }
 
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#put(String,Object)
      */
-    public Object put(String key, Object value) {
-        return _doc.put(key, value);
+    public Object put( String key , Object value ){
+        return _doc.put( key, value );
     }
-
 
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#putAll(BSONObject)
      */
-    public void putAll(BSONObject otherDoc) {
-        _doc.putAll(otherDoc);
+    public void putAll( BSONObject otherDoc ){
+        _doc.putAll( otherDoc );
     }
-
 
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#putAll(Map)
      */
-    public void putAll(Map otherMap) {
-        _doc.putAll(otherMap);
+    public void putAll( Map otherMap ){
+        _doc.putAll( otherMap );
     }
-
 
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#get(String)
      */
-    public Object get(String key) {
-        return _doc.get(key);
+    public Object get( String key ){
+        return _doc.get( key );
     }
-
 
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#toMap()
      */
-    public Map toMap() {
+    public Map toMap(){
         return _doc.toMap();
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#removeField(String)
      */
-    public Object removeField(String key) {
+    public Object removeField( String key ){
         return _doc.removeField( key );
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#containsKey(String)
      */
-    public boolean containsKey(String key) {
+    public boolean containsKey( String key ){
         return _doc.containsKey( key );
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#containsField(String)
      */
-    public boolean containsField(String fieldName) {
+    public boolean containsField( String fieldName ){
         return _doc.containsField( fieldName );
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see BSONObject#keySet()
      */
-    public Set<java.lang.String> keySet() {
+    public Set<java.lang.String> keySet(){
         return _doc.keySet();
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see Writable#write(DataOutput)
      */
-    public void write(DataOutput out)
-      throws IOException {
+    public void write( DataOutput out ) throws IOException{
         BSONEncoder enc = new BSONEncoder();
         BasicOutputBuffer buf = new BasicOutputBuffer();
         enc.set( buf );
         enc.putObject( _doc );
+        enc.done();
         out.write( buf.toByteArray() );
     }
 
-
     /**
      * {@inheritDoc}
+     * 
      * @see Writable#readFields(DataInput)
      */
-    public void readFields(DataInput in)
-      throws IOException {
+    public void readFields( DataInput in ) throws IOException{
         BSONDecoder dec = new BSONDecoder();
         BSONCallback cb = new BasicBSONCallback();
         // Read the BSON length from the start of the record
@@ -151,6 +155,21 @@ public class BSONWritable implements BSONObject, Writable {
         byte[] buf = new byte[dataLen];
         in.readFully( buf );
         dec.decode( buf, cb );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString(){
+        BSONEncoder enc = new BSONEncoder();
+        BasicOutputBuffer buf = new BasicOutputBuffer();
+        enc.set( buf );
+        enc.putObject( _doc );
+        enc.done();
+        String str = buf.asString();
+        log.debug( "Output As String: '" + str + "'" );
+        return str;
     }
 
     final BSONObject _doc;
