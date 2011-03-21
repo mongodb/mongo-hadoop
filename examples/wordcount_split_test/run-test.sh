9f4ee05ebec242b58e99c5296f4ad5c642709816
Joseph Shraibman <jks@iname.com>
2011-03-21 18:44:54 -0400

    Pass through command line arguments to java program

--- examples/wordcount_split_test/run-test.sh
+++ examples/wordcount_split_test/run-test.sh
@@ -4,6 +4,8 @@ export HADOOP_CLASSPATH=
 cd `dirname $0`
 jar -cf wc-split-test.jar *.class
 
-hadoop jar ./wc-split-test.jar  WordCountSplitTest
+hadoop jar ./wc-split-test.jar  WordCountSplitTest $*
+
+#  -D mapred.job.tracker=local 
 
 
