c5da702960cbb1e9bb1aab8d4a9ac9d5f820aa43
Joseph Shraibman <jks@iname.com>
2011-03-29 00:21:40 -0400

    Extend MongoTool so can be run by test harness

--- examples/wordcount_split_test/src/WordCountSplitTest.java
+++ examples/wordcount_split_test/src/WordCountSplitTest.java
@@ -26,7 +26,7 @@ import org.bson.*;
 import com.mongodb.hadoop.*;
 import com.mongodb.hadoop.util.*;
 
-public class WordCountSplitTest {
+public class WordCountSplitTest extends MongoTool {
 
     private static final Log log = LogFactory.getLog( WordCountSplitTest.class );
         private static boolean did_start = false;
@@ -73,6 +73,86 @@ public class WordCountSplitTest {
             context.write( key, result );
         }
     }
+
+    public int run(String[] args) throws Exception {
+         final Configuration conf = getConf();
+          boolean useQuery = false;
+        for(int i = 0 ; i < args.length ; i++){
+            final String argi = args[i];
+            if (argi.equals("--use-query"))
+                useQuery = true;
+            else{
+                throw new IllegalArgumentException(argi);
+            }
+        }
+
+
+        if(useQuery) {
+            //NOTE: must do this BEFORE Job is created
+            final MongoConfig mongo_conf = new MongoConfig(conf);
+            com.mongodb.BasicDBObject query = new com.mongodb.BasicDBObject();
+            query.put( "num", new com.mongodb.BasicDBObject(Collections.singletonMap("$mod", new int[]{2,0})) );
+            System.out.println(" --- setting query on num");
+            mongo_conf.setQuery(query);
+            System.out.println(" --- query is: "+mongo_conf.getQuery());
+        }
+
+
+        final com.mongodb.MongoURI outputUri =  MongoConfigUtil.getOutputURI( conf);
+        if (outputUri == null)
+            throw new IllegalStateException("output uri is not set");
+        if (MongoConfigUtil.getInputURI(conf) == null)
+            throw new IllegalStateException("input uri is not set");
+        final String outputCollectionName = outputUri.getCollection();
+
+        final Job job = new Job( conf , "word count "+outputCollectionName );
+
+        job.setJarByClass( WordCountSplitTest.class );
+
+        job.setMapperClass( TokenizerMapper.class );
+
+        job.setCombinerClass( IntSumReducer.class );
+        job.setReducerClass( IntSumReducer.class );
+
+        job.setOutputKeyClass( Text.class );
+        job.setOutputValueClass( IntWritable.class );
+
+        job.setInputFormatClass( MongoInputFormat.class );
+        job.setOutputFormatClass( MongoOutputFormat.class );
+
+        final long start = System.currentTimeMillis();
+        System.out.println(" ----------------------- running test "+outputCollectionName +" --------------------");
+        try{
+            boolean result = job.waitForCompletion( true );
+            System.out.println("job.waitForCompletion( true ) returned "+result);
+        }catch(Exception e){
+            System.err.println("job.waitForCompletion( true ) threw Exception");
+            e.printStackTrace();
+            return 1;
+        }
+        final long end = System.currentTimeMillis();
+        final float seconds = ((float)(end - start))/1000;
+        java.text.NumberFormat nf = java.text.NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(3);
+        System.out.println("finished run in "+nf.format(seconds)+" seconds");
+
+        com.mongodb.Mongo m = new com.mongodb.Mongo( outputUri);
+        com.mongodb.DB db = m.getDB( outputUri.getDatabase() );
+        com.mongodb.DBCollection coll = db.getCollection(outputCollectionName);
+        com.mongodb.BasicDBObject query = new com.mongodb.BasicDBObject();
+        query.put( "_id","the");
+        com.mongodb.DBCursor cur = coll.find(query);
+        if (! cur.hasNext())
+            System.out.println("FAILURE: could not find count of \'the\'");
+        else
+            System.out.println("'the' count: "+cur.next());
+
+        return 0; //is the return value supposed to be the program exit code?
+
+//        if (! result)
+//           System.exit(  1 );
+    }
+
     private final static void test(boolean useShards, boolean useChunks, Boolean slaveok, boolean useQuery) throws Exception{
         did_start = false;
         final Configuration conf = new Configuration();
