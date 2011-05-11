aae89d8ac3b8354165f0e57f52054900fc9e9d2f
Joseph Shraibman <jks@iname.com>
2011-05-11 09:38:08 -0400

    Copy ip db text file to hdfs

--- examples/ip_location/download-ip-db.sh
+++ examples/ip_location/download-ip-db.sh
@@ -6,3 +6,5 @@ if ! [ -f IpToCountry.csv ] ; then
  wget software77.net/geo-ip/?DL=1 -O IpToCountry.csv.gz && gunzip IpToCountry.csv.gz
 fi
  
+
+hadoop fs -copyFromLocal  IpToCountry.csv IpToCountry.csv
