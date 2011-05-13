9bf9b88c3c2575ffdd69ad4e95334639f807c028
Joseph Shraibman <jks@iname.com>
2011-05-13 11:21:57 -0400

    cleanup

--- examples/ip_location/src/ApacheLogFileReader.java
+++ examples/ip_location/src/ApacheLogFileReader.java
@@ -5,8 +5,7 @@ import com.mongodb.MongoURI;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,16 +18,16 @@ import java.util.regex.Pattern;
 public class ApacheLogFileReader {
      
     private void connect() throws MongoException, UnknownHostException{
-         com.mongodb.MongoURI uri = new MongoURI(_uriString);
-                    com.mongodb.Mongo mongo = uri.connect();
-                        com.mongodb.DB db = mongo.getDB(uri.getDatabase());
-                        _coll = db.getCollection(uri.getCollection());
+        com.mongodb.MongoURI uri = new MongoURI(_uriString);
+        com.mongodb.Mongo mongo = uri.connect();
+        com.mongodb.DB db = mongo.getDB(uri.getDatabase());
+        _coll = db.getCollection(uri.getCollection());
     }
     private interface logParser{
         Matcher match(String logline);
         List<String> getKeys();
     }
-    private logParser commonApacheParser = new logParser() {
+    private final static logParser commonApacheParser = new logParser() {
         private final Pattern pattern = Pattern.compile("^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) ((\\d+)|-)");// \"([^\"]+)\" \"([^\"]+)\"";
 
         public Matcher match(String logline) {
@@ -59,11 +58,9 @@ public class ApacheLogFileReader {
             return keyList;
         }
     };
-    private List<logParser> _logParsersToTry = Arrays.asList(vcombinedApacheParser, combinedApacheParser, commonApacheParser);
-    private logParser _logParser = null;
+    private final static List<logParser> _logParsersToTry = Collections.unmodifiableList(Arrays.asList(vcombinedApacheParser, combinedApacheParser, commonApacheParser));
     
     // 192.168.1.106 - - [24/Jun/2007:00:27:12 -0400] "GET /temp.html HTTP/1.1" 304 - "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4"
-    private int _badLinesCount = 0;
     private void insert(String logline){
         if (_logParser == null){
             //try patterns against our log file until we find one that fits
@@ -81,16 +78,14 @@ public class ApacheLogFileReader {
             _badLinesCount++;
             return;
         }
-        
-
-     BasicDBObject doc = new BasicDBObject();
-     int key_idx = 0;
-     for(String key : _logParser.getKeys()){
-         key_idx++; //first key has index of 1
-         if (key != null && ! ("-".equals(key) || "\"-\"".equals(key) ) )
-             doc.append(key, m.group(key_idx));
-     }
-            _coll.insert(doc);
+        BasicDBObject doc = new BasicDBObject();
+        int key_idx = 0;
+        for(String key : _logParser.getKeys()){
+            key_idx++; //first key has index of 1
+            if (key != null && ! ("-".equals(key) || "\"-\"".equals(key) ) )
+                doc.append(key, m.group(key_idx));
+        }
+        _coll.insert(doc);
     }
     public static final void main(String[] args) throws Exception {
         ApacheLogFileReader fr = new ApacheLogFileReader();
@@ -108,6 +103,9 @@ public class ApacheLogFileReader {
         }
         System.out.println("Read "+linecount+" lines from logfile, could not parse "+fr._badLinesCount+" of them");
     }
+
     private String _uriString;
     private com.mongodb.DBCollection _coll ;
+    private logParser _logParser = null;
+    private int _badLinesCount = 0;
 }
