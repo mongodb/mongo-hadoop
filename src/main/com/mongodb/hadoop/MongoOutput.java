4b10f2273036e11411c9d976a7dedb71b34e33c7
Ryan <rgnitz@gmail.com>
2011-05-19 18:32:04 -0400

    layout changes - test commit.

--- src/main/com/mongodb/hadoop/MongoOutput.java
+++ src/main/com/mongodb/hadoop/MongoOutput.java
@@ -1,13 +1,12 @@
-// MongoOutput.java
 /*
- * Copyright 2010 10gen Inc.
- * 
+ * Copyright 2011 10gen Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +19,9 @@ package com.mongodb.hadoop;
 import com.mongodb.*;
 
 public interface MongoOutput {
+
     public void appendAsKey( DBObject o );
 
     public void appendAsValue( DBObject o );
+
 }
