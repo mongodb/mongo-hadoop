b05449c50382f086d4422df677a6b58af992856c
Ryan <rgnitz@gmail.com>
2011-05-20 16:20:47 -0400

    layout change (ws)

--- src/main/com/mongodb/hadoop/input/MongoInputSplit.java
+++ src/main/com/mongodb/hadoop/input/MongoInputSplit.java
@@ -1,13 +1,13 @@
 // MongoInputSplit.java
 /*
  * Copyright 2010 10gen Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,7 +86,7 @@ public class MongoInputSplit extends InputSplit implements Writable, org.apache.
         _limit = in.readInt();
         _skip = in.readInt();
 
-        log.info( "Deserialized MongoInputSplit ... { length = " + getLength() + ", locations = " + java.util.Arrays.toString(getLocations()) + ", query = " + _querySpec
+        log.info( "Deserialized MongoInputSplit ... { length = " + getLength() + ", locations = " + Arrays.toString(getLocations()) + ", query = " + _querySpec
                 + ", fields = " + _fieldSpec + ", sort = " + _sortSpec + ", limit = " + _limit + ", skip = " + _skip + "}" );
 
         objIn.close();
