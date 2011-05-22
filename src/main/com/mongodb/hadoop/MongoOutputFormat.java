9de03cd44966e7b8feada71e27755e37e8b9cc32
Ryan <rgnitz@gmail.com>
2011-05-22 18:37:31 -0400

    layout and added comments.

--- src/main/com/mongodb/hadoop/MongoOutputFormat.java
+++ src/main/com/mongodb/hadoop/MongoOutputFormat.java
@@ -1,13 +1,12 @@
-// MongoOutputFormat.java
 /*
  * Copyright 2010 10gen Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,27 +16,39 @@
 
 package com.mongodb.hadoop;
 
-import org.apache.commons.logging.*;
-import org.apache.hadoop.mapreduce.*;
+// Mongo
+import com.mongodb.hadoop.output.MongoOutputCommiter;
+import com.mongodb.hadoop.output.MongoRecordWriter;
+import com.mongodb.hadoop.util.MongoConfigUtil;
 
-import com.mongodb.hadoop.output.*;
-import com.mongodb.hadoop.util.*;
+// Commons
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+// Hadoop
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.OutputFormat;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.OutputCommitter;
+import org.apache.hadoop.mapreduce.RecordWriter;
 
 public class MongoOutputFormat<K,V> extends OutputFormat<K, V> {
-    private static final Log log = LogFactory.getLog( MongoOutputFormat.class );
 
-    public MongoOutputFormat() {
-    }
+    public void checkOutputSpecs( final JobContext context ) { }
 
-    public void checkOutputSpecs( JobContext context ){
-    }
-
-    public OutputCommitter getOutputCommitter( TaskAttemptContext context ){
+    public OutputCommitter getOutputCommitter( final TaskAttemptContext context ) {
         return new MongoOutputCommiter();
     }
 
-    public RecordWriter<K, V> getRecordWriter( TaskAttemptContext context ){
+    /**
+     * Get the record writer that points to the output collection.
+     */
+    public RecordWriter<K, V> getRecordWriter( final TaskAttemptContext context ) {
         return new MongoRecordWriter( MongoConfigUtil.getOutputCollection( context.getConfiguration() ) , context );
     }
 
+    public MongoOutputFormat() { }
+
+    private static final Log LOG = LogFactory.getLog( MongoOutputFormat.class );
 }
+
