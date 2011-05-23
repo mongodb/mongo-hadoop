16d5dc614dcd0e79cb8edec72c0f52e2e2343b55
Brendan W. McAdams <brendan@10gen.com>
2011-05-23 16:01:19 -0400

    Conform to 10gen Java Standards by having all variables in ONE PLACE (to start with) and AT THE END OF THE FILE.

--- src/main/com/mongodb/hadoop/input/MongoRecordReader.java
+++ src/main/com/mongodb/hadoop/input/MongoRecordReader.java
@@ -26,7 +26,6 @@ import com.mongodb.*;
 
 public class MongoRecordReader extends RecordReader<Object, BSONObject>
    implements org.apache.hadoop.mapred.RecordReader<org.apache.hadoop.io.ObjectWritable, com.mongodb.hadoop.io.BSONWritable> {
-    private static final Log log = LogFactory.getLog( MongoRecordReader.class );
 
     public MongoRecordReader(MongoInputSplit split) {
         _split = split;
@@ -60,16 +59,10 @@ public class MongoRecordReader extends RecordReader<Object, BSONObject>
         _cur = _cursor.next();
         _seen++;
         return true;
+
     }
 
-    final MongoInputSplit _split;
-    final DBCursor _cursor;
-
-    BSONObject _cur;
-    float _seen = 0;
-    float _total;
-    //---------------------------------
-    //Methods to implement org.apache.hadoop.mapred.RecordReader, originally in com.mongodb.hadoop.mapred.input.MongoRecordReader
+    // Methods to implement org.apache.hadoop.mapred.RecordReader, originally in com.mongodb.hadoop.mapred.input.MongoRecordReader
     public boolean next(org.apache.hadoop.io.ObjectWritable key,  com.mongodb.hadoop.io.BSONWritable value) {
         if (nextKeyValue()) {
             log.debug("Had another k/v");
@@ -95,5 +88,14 @@ public class MongoRecordReader extends RecordReader<Object, BSONObject>
     public long getPos() {
         return new Float(_seen).longValue();
     }
-    
+
+    final MongoInputSplit _split;
+    final DBCursor _cursor;
+
+    BSONObject _cur;
+    float _seen = 0;
+    float _total;
+
+    private static final Log log = LogFactory.getLog( MongoRecordReader.class );
+
 }
