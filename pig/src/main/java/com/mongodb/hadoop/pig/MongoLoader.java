ee38cb3d4eeaa480b0257edcbb3cb55971f6ea49
Jeremy Karn <jkarn@mortardata.com>
2012-10-05 16:32:26 -0400

    Gracefully handle errors applying schema to Mongo objects, Recursively apply schema to map objects, and add tests for all mongo -> pig conversions.

--- pig/src/main/java/com/mongodb/hadoop/pig/MongoLoader.java
+++ pig/src/main/java/com/mongodb/hadoop/pig/MongoLoader.java
@@ -1,35 +1,36 @@
 package com.mongodb.hadoop.pig;
 
-import com.mongodb.*;
-import com.mongodb.hadoop.*;
-import com.mongodb.hadoop.input.*;
-import com.mongodb.hadoop.output.MongoRecordWriter;
-import com.mongodb.hadoop.util.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.logging.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.pig.*;
-import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
-import org.apache.pig.data.*;
-import org.apache.pig.impl.logicalLayer.FrontendException;
-import org.apache.pig.impl.util.*;
-import org.apache.pig.parser.ParserException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.pig.Expression;
+import org.apache.pig.LoadFunc;
 import org.apache.pig.LoadMetadata;
-import org.apache.pig.LoadPushDown;
 import org.apache.pig.ResourceSchema;
-import org.apache.pig.LoadPushDown.RequiredField;
-import org.apache.pig.LoadPushDown.RequiredFieldResponse;
 import org.apache.pig.ResourceSchema.ResourceFieldSchema;
 import org.apache.pig.ResourceStatistics;
+import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.util.Utils;
+import org.apache.pig.parser.ParserException;
 import org.bson.BSONObject;
 
-
-import java.io.*;
-import java.text.ParseException;
-import java.util.*;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.hadoop.MongoInputFormat;
+import com.mongodb.hadoop.input.MongoRecordReader;
+import com.mongodb.hadoop.util.MongoConfigUtil;
 
 public class MongoLoader extends LoadFunc implements LoadMetadata {
 	private static final Log log = LogFactory.getLog( MongoStorage.class );
@@ -101,60 +102,79 @@ public class MongoLoader extends LoadFunc implements LoadMetadata {
 		
         
 	}
-	private Object readField(Object obj, ResourceFieldSchema field) throws IOException {
+	
+	protected Object readField(Object obj, ResourceFieldSchema field) throws IOException {
 		if(obj == null)
 			return null;
 		
-		switch (field.getType()) {
-		case DataType.INTEGER:
-			return Integer.parseInt(obj.toString());
-		case DataType.LONG:
-			return Long.parseLong(obj.toString());
-		case DataType.FLOAT:
-			return Float.parseFloat(obj.toString());
-		case DataType.DOUBLE:
-			return Double.parseDouble(obj.toString());
-		case DataType.BYTEARRAY:
-			return obj;
-		case DataType.CHARARRAY:
-			return obj.toString();
-		case DataType.TUPLE:
-			ResourceSchema s = field.getSchema();
-			ResourceFieldSchema[] fs = s.getFields();
-			Tuple t = tupleFactory.newTuple(fs.length);
-			
-			BasicDBObject val = (BasicDBObject)obj;
-			
-			for(int j = 0; j < fs.length; j++) {
-				t.set(j, readField(val.get(fs[j].getName()) ,fs[j]));
-			}
-			
-			return t;
-			
-		case DataType.BAG:
-			s = field.getSchema();
-			fs = s.getFields();
-			
-			s = fs[0].getSchema();
-			fs = s.getFields();
-			
-			DataBag bag = bagFactory.newDefaultBag();
-			
-			BasicDBList vals = (BasicDBList)obj;
-						
-			for(int j = 0; j < vals.size(); j++) {
-				
-				t = tupleFactory.newTuple(fs.length);
-				for(int k = 0; k < fs.length; k++) {
-					t.set(k, readField(((BasicDBObject)vals.get(j)).get(fs[k].getName()), fs[k]));
-				}
-				bag.add(t);
-			}
-			
-			
-			return bag;
-		default:
-			return obj;
+		try {
+    		switch (field.getType()) {
+    		case DataType.INTEGER:
+    			return Integer.parseInt(obj.toString());
+    		case DataType.LONG:
+    			return Long.parseLong(obj.toString());
+    		case DataType.FLOAT:
+    			return Float.parseFloat(obj.toString());
+    		case DataType.DOUBLE:
+    			return Double.parseDouble(obj.toString());
+    		case DataType.BYTEARRAY:
+    			return obj;
+    		case DataType.CHARARRAY:
+    			return obj.toString();
+    		case DataType.TUPLE:
+    			ResourceSchema s = field.getSchema();
+    			ResourceFieldSchema[] fs = s.getFields();
+    			Tuple t = tupleFactory.newTuple(fs.length);
+    			
+    			BasicDBObject val = (BasicDBObject)obj;
+    			
+    			for(int j = 0; j < fs.length; j++) {
+    				t.set(j, readField(val.get(fs[j].getName()) ,fs[j]));
+    			}
+    			
+    			return t;
+    			
+    		case DataType.BAG:
+    			s = field.getSchema();
+    			fs = s.getFields();
+    			
+    			s = fs[0].getSchema();
+    			fs = s.getFields();
+    			
+    			DataBag bag = bagFactory.newDefaultBag();
+    			
+    			BasicDBList vals = (BasicDBList)obj;
+    						
+    			for(int j = 0; j < vals.size(); j++) {
+    				t = tupleFactory.newTuple(fs.length);
+    				for(int k = 0; k < fs.length; k++) {
+    					t.set(k, readField(((BasicDBObject)vals.get(j)).get(fs[k].getName()), fs[k]));
+    				}
+    				bag.add(t);
+    			}
+    			
+    			return bag;
+    		case DataType.MAP:
+                s = field.getSchema();
+                fs = s.getFields();
+                
+                BasicDBObject inputMap = (BasicDBObject) obj;
+                
+                Map outputMap = new HashMap();
+                for (String key : inputMap.keySet()) {
+                    outputMap.put(key, readField(inputMap.get(key), fs[0]));
+                }
+                return outputMap;
+
+    		    
+    		default:
+    			return obj;
+    		}
+		} catch (Exception e) {
+		    String fieldName = field.getName() == null ? "" : field.getName();
+		    String type = DataType.genTypeToNameMap().get(field.getType());
+		    log.warn("Type " + type + " for field " + fieldName + " can not be applied to " + obj.toString() + ".  Returning null.");
+		    return null;
 		}
 		
 	}
