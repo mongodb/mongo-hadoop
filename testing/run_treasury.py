8be74b8303e3f6c0b22453740929446df5f5f469
Mike O'Brien <mikeo@10gen.com>
2012-12-18 17:07:32 -0500

    refactor testing to make it more concise.

--- testing/run_treasury.py
+++ testing/run_treasury.py
@@ -40,6 +40,22 @@ parameters = {
   "mongo.job.sort_comparator":"",
 }
 
+def runjob(hostname):
+    cmd = [os.path.join(HADOOP_HOME, "bin", "hadoop")]
+    cmd.append("jar")
+    cmd.append(JOBJAR_PATH)
+
+    for key, val in parameters.items():
+        cmd.append("-D")
+        cmd.append(key + "=" + val)
+
+    cmd.append("-D")
+    cmd.append("mongo.input.uri=mongodb://%s/mongo_hadoop.yield_historical.in?readPreference=primary" % hostname)
+    cmd.append("-D")
+    cmd.append("mongo.output.uri=mongodb://%s/mongo_hadoop.yield_historical.out" % hostname)
+
+    subprocess.call(' '.join(cmd), shell=True)
+
 
 class TestBasic(unittest.TestCase):
     
@@ -54,24 +70,9 @@ class TestBasic(unittest.TestCase):
         print "server is ready."
 
     def test_treasury(self):
-        print "running treasury example test."
-        cmd = [os.path.join(HADOOP_HOME, "bin", "hadoop")]
-        cmd.append("jar")
-        cmd.append(JOBJAR_PATH)
-        #cmd.append("com.mongodb.hadoop.examples.treasury.TreasuryYieldXMLConfig")
-
-        for key, val in parameters.items():
-            cmd.append("-D")
-            cmd.append(key + "=" + val)
-
-        cmd.append("-D")
-        cmd.append("mongo.input.uri=mongodb://%s/mongo_hadoop.yield_historical.in" % self.server_hostname)
-        cmd.append("-D")
-        cmd.append("mongo.output.uri=mongodb://%s/mongo_hadoop.yield_historical.out" % self.server_hostname)
-
-        subprocess.call(' '.join(cmd), shell=True)
-        self.assertEqual( self.server.connection()['mongo_hadoop']['yield_historical.out'].count(), 21 )
-        print ' '.join(cmd)
+        runjob(self.server_hostname)
+        out_col = self.server.connection()['mongo_hadoop']['yield_historical.out']
+        self.assertEqual( out_col.count(), 21 )
 
     def tearDown(self):
         print "killing it!"
@@ -83,15 +84,20 @@ class TestBasic(unittest.TestCase):
 class TestSharded(unittest.TestCase):
 
     def setUp(self):
-        self.shard1 = mongo_manager.ReplicaSetManager(home="/tmp/rs0", with_arbiter=True, num_members=3)
+        self.shard1 = mongo_manager.ReplicaSetManager(home="/tmp/rs0",
+                with_arbiter=True,
+                num_members=3)
         self.shard1.start_set(fresh=True)
-        self.shard2 = mongo_manager.ReplicaSetManager(home="/tmp/rs1", with_arbiter=True, num_members=3)
+        self.shard2 = mongo_manager.ReplicaSetManager(home="/tmp/rs1",
+                with_arbiter=True,
+                num_members=3)
         self.shard2.start_set(fresh=True)
         self.configdb = mongo_manager.StandaloneManager(home="/tmp/config_db")  
         self.confighost = self.configdb.start_server(fresh=True)
 
         self.mongos = mongo_manager.MongosManager(home="/tmp/mongos")
-        self.mongos_hostname = self.mongos.start_mongos(self.confighost, [h.get_shard_string() for h in (self.shard1,self.shard2)],
+        self.mongos_hostname = self.mongos.start_mongos(self.confighost,
+                [h.get_shard_string() for h in (self.shard1,self.shard2)],
                 noauth=False, fresh=True, addShards=True)
 
         self.mongos_connection = self.mongos.connection()
@@ -100,50 +106,28 @@ class TestSharded(unittest.TestCase):
                                    "mongo_hadoop",
                                    "yield_historical.in",
                                    JSONFILE_PATH)
-        self.mongos_connection['admin'].command("enablesharding", "mongo_hadoop")
-        self.mongos_connection['admin'].command("shardCollection", "mongo_hadoop.yield_historical.in", key={"_id":1})
-        self.mongos_connection['admin'].command("split", "mongo_hadoop.yield_historical.in", find={"_id":1})
+        mongos_admindb = self.mongos_connection['admin']
+        mongos_admindb.command("enablesharding", "mongo_hadoop")
+        mongos_admindb.command("shardCollection",
+                "mongo_hadoop.yield_historical.in",
+                key={"_id":1})
+        mongos_admindb.command("split",
+                "mongo_hadoop.yield_historical.in",
+                find={"_id":1})
 
     def test_treasury(self):
-        print "running treasury example test."
-        cmd = [os.path.join(HADOOP_HOME, "bin", "hadoop")]
-        cmd.append("jar")
-        cmd.append(JOBJAR_PATH)
-
-        for key, val in parameters.items():
-            cmd.append("-D")
-            cmd.append(key + "=" + val)
-
-        cmd.append("-D")
-        cmd.append("mongo.input.uri=mongodb://%s/mongo_hadoop.yield_historical.in?readPreference=primary" % self.mongos_hostname)
-        cmd.append("-D")
-        cmd.append("mongo.output.uri=mongodb://%s/mongo_hadoop.yield_historical.out" % self.mongos_hostname)
-
-        subprocess.call(' '.join(cmd), shell=True)
-        self.assertEqual( self.mongos_connection['mongo_hadoop']['yield_historical.out'].count(), 21 )
-        print ' '.join(cmd)
+        runjob(self.mongos_hostname)
+        out_col = self.mongos_connection['mongo_hadoop']['yield_historical.out']
+        self.assertEqual( out_col.count(), 21 )
 
     def tearDown(self):
         print "killing servers!"
-        self.mongos.kill_all_members()
-        self.shard1.kill_all_members()
-        self.shard2.kill_all_members()
-        self.configdb.kill_all_members()
+        #self.mongos.kill_all_members()
+        #self.shard1.kill_all_members()
+        #self.shard2.kill_all_members()
+        #self.configdb.kill_all_members()
 
-#def testtreasury():
-        #cmd = [os.path.join(HADOOP_HOME, "bin", "hadoop")]
-        #cmd.append("jar")
-        #cmd.append("/Users/mike/projects/mongo-hadoop/examples/treasury_yield/target/treasury-example_1.0.3-1.1.0-SNAPSHOT.jar")
-
-        #for key, val in parameters.items():
-            #cmd.append("-D")
-            #cmd.append(key + "=" + val)
-#
-        #cmd.append("-D")
-        #cmd.append("mongo.input.uri=mongodb://localhost:4007/mongo_hadoop.yield_historical.in")
-        #cmd.append("-D")
-        #cmd.append("mongo.output.uri=mongodb://localhost:4007/mongo_hadoop.yield_historical.out")
-
-        #subprocess.call(cmd)
+def testtreasury():
+    runjob('localhost:4007')
 
 if __name__ == '__main__': testtreasury()
