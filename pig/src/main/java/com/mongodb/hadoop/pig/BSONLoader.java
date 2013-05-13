bf361963829b4c1e42f4a9668660b6c8f0f6e826
Mike O'Brien <mikeo@10gen.com>
2013-05-13 17:05:29 -0400

    move static readField and convertBSONtopigtuple functions into BSONLoader. add schema + schemaless support to BSONLoader

--- pig/src/main/java/com/mongodb/hadoop/pig/BSONLoader.java
+++ pig/src/main/java/com/mongodb/hadoop/pig/BSONLoader.java
@@ -14,6 +14,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.pig.ResourceSchema;
+import org.apache.pig.ResourceSchema.ResourceFieldSchema;
 import org.apache.pig.Expression;
 import org.apache.pig.LoadFunc;
 import org.apache.pig.backend.executionengine.ExecException;
@@ -26,16 +28,42 @@ import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 import org.bson.*;
 import org.bson.types.*;
+import org.apache.pig.impl.util.Utils;
 
 public class BSONLoader extends LoadFunc {
 
-    private static TupleFactory tf = TupleFactory.getInstance();
+    private static TupleFactory tupleFactory = TupleFactory.getInstance();
+	private static BagFactory bagFactory = BagFactory.getInstance();
     private static final Log log = LogFactory.getLog(BSONLoader.class);
     private String location;
     private final BSONFileInputFormat inputFormat = new BSONFileInputFormat();
     protected RecordReader in = null;
 
+    private static final String PIG_INPUT_SCHEMA_UDF_CONTEXT = "bson.pig.input.schema.udf_context";
+    private String _udfContextSignature = null;
+    private ResourceFieldSchema[] fields;
+    protected ResourceSchema schema = null;
+    private String idAlias = null;
+
+
     public BSONLoader(){}
+
+    public BSONLoader (String idAlias, String userSchema) {
+        this.idAlias = idAlias;
+    	try {
+			schema = new ResourceSchema(Utils.getSchemaFromString(userSchema));
+			fields = schema.getFields();
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Invalid Schema Format");
+		}
+    }
+
+
+    @Override
+    public void setUDFContextSignature( String signature ){
+        _udfContextSignature = signature;
+    }
+    
 
     @Override
     public void setLocation(String location, Job job) throws IOException{
@@ -55,11 +83,27 @@ public class BSONLoader extends LoadFunc {
 
     @Override
     public Tuple getNext() throws IOException{
+		BSONObject val;
         try{
             if(!in.nextKeyValue()) return null;
-            Object val = in.getCurrentValue();
-            Tuple t = tf.newTuple(1);
-            t.set(0, convertBSONtoPigType(val));
+            val = (BSONObject)in.getCurrentValue();
+
+            Tuple t;
+            if( this.fields == null ){
+                // Schemaless mode - just output a tuple with a single element,
+                // which is a map storing the keys/vals in the document
+                t = tupleFactory.newTuple(1);
+                t.set(0, BSONLoader.convertBSONtoPigType(val));
+            }else{
+                t = tupleFactory.newTuple(fields.length);
+                for(int i = 0; i < fields.length; i++) {
+                    String fieldTemp = fields[i].getName();
+                    if(this.idAlias != null && this.idAlias.equals(fieldTemp)){
+                        fieldTemp = "_id";
+                    }
+                    t.set(i, BSONLoader.readField(val.get(fieldTemp), fields[i]));
+                }
+            }
             return t;
         } catch (InterruptedException e) {
             int errCode = 6018;
@@ -67,6 +111,91 @@ public class BSONLoader extends LoadFunc {
         }
 
     }
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    protected static Object readField(Object obj, ResourceFieldSchema field) throws IOException {
+		if(obj == null)
+			return null;
+		
+		try {
+			if(field == null)
+				return obj;
+
+    		switch (field.getType()) {
+    		case DataType.INTEGER:
+    			return Integer.parseInt(obj.toString());
+    		case DataType.LONG:
+    			return Long.parseLong(obj.toString());
+    		case DataType.FLOAT:
+    			return Float.parseFloat(obj.toString());
+    		case DataType.DOUBLE:
+    			return Double.parseDouble(obj.toString());
+    		case DataType.BYTEARRAY:
+                return BSONLoader.convertBSONtoPigType(obj);
+    		case DataType.CHARARRAY:
+    			return obj.toString();
+    		case DataType.TUPLE:
+    			ResourceSchema s = field.getSchema();
+    			ResourceFieldSchema[] fs = s.getFields();
+    			Tuple t = tupleFactory.newTuple(fs.length);
+    			
+    			BasicDBObject val = (BasicDBObject)obj;
+    			
+    			for(int j = 0; j < fs.length; j++) {
+    				t.set(j, readField(val.get(fs[j].getName()) ,fs[j]));
+    			}
+    			
+    			return t;
+    			
+    		case DataType.BAG:
+    			s = field.getSchema();
+    			fs = s.getFields();
+    			
+    			s = fs[0].getSchema();
+    			fs = s.getFields();
+    			
+    			DataBag bag = bagFactory.newDefaultBag();
+    			
+    			BasicDBList vals = (BasicDBList)obj;
+    						
+    			for(int j = 0; j < vals.size(); j++) {
+    				t = tupleFactory.newTuple(fs.length);
+    				for(int k = 0; k < fs.length; k++) {
+    					t.set(k, readField(((BasicDBObject)vals.get(j)).get(fs[k].getName()), fs[k]));
+    				}
+    				bag.add(t);
+    			}
+    			
+    			return bag;
+
+    		case DataType.MAP:
+                s = field.getSchema();
+                fs = s != null ? s.getFields() : null;
+                BasicDBObject inputMap = (BasicDBObject) obj;
+                
+                Map outputMap = new HashMap();
+                for (String key : inputMap.keySet()) {
+					if(fs != null){
+						outputMap.put(key, readField(inputMap.get(key), fs[0]));
+					}else{
+						outputMap.put(key, readField(inputMap.get(key), null));
+					}
+                }
+                return outputMap;
+
+    		default:
+                log.info("asfkjabskfjbsaf default for "+ field.getName());
+                return BSONLoader.convertBSONtoPigType(obj);
+    		}
+		} catch (Exception e) {
+		    String fieldName = field.getName() == null ? "" : field.getName();
+		    String type = DataType.genTypeToNameMap().get(field.getType());
+		    log.warn("Type " + type + " for field " + fieldName + " can not be applied to " + obj.toString() + ".  Returning null.");
+		    return null;
+		}
+		
+	}
+	
 
     public static Object convertBSONtoPigType(Object o) throws ExecException{
         if(o == null){
@@ -79,7 +208,7 @@ public class BSONLoader extends LoadFunc {
             return o.toString();
         } else if(o instanceof BasicBSONList){
             BasicBSONList bl = (BasicBSONList)o;
-            Tuple t = tf.newTuple(bl.size());
+            Tuple t = tupleFactory.newTuple(bl.size());
             for(int i = 0; i < bl.size(); i++){
                 t.set(i, convertBSONtoPigType(bl.get(i)));
             }
