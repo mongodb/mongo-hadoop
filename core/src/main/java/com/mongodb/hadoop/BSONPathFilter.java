501afd1bf6d7030a4f00a4c7af5c778fad2e8e4e
Mike O'Brien <mikeo@10gen.com>
2013-05-30 18:16:29 -0400

    new file BSONPathFilter for filtering just the non-hidden files with .bson extension.

---
+++ core/src/main/java/com/mongodb/hadoop/BSONPathFilter.java
@@ -0,0 +1,21 @@
+package com.mongodb.hadoop;
+import org.apache.commons.logging.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+
+public class BSONPathFilter implements PathFilter{
+
+    private static final Log log = LogFactory.getLog( BSONPathFilter.class );
+
+    public BSONPathFilter(){
+        log.info("path filter constructed.");
+    }
+
+    public boolean accept(Path path){
+        String pathName = path.getName().toLowerCase();
+        boolean acceptable = pathName.endsWith(".bson") && !pathName.startsWith("."); 
+        log.info("returning " + acceptable);
+        return acceptable;
+    }
+}
+
