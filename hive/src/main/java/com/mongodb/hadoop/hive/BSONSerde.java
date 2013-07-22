304bb7c7e34b5ec3232ee0f39c578a3b8c7cdc6e
Sweet Song <shihui.sweet.song@gmail.com>
2013-07-22 16:54:51 -0400

    changed incorrect path and updated license year

--- hive/src/main/java/com/mongodb/hadoop/hive/BSONSerde.java
+++ hive/src/main/java/com/mongodb/hadoop/hive/BSONSerde.java
@@ -1,7 +1,7 @@
 package com.mongodb.hadoop.hive;
 
 /**
- * Copyright (c) 2008 - 2012 10gen, Inc. <http://10gen.com>
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
