62b9b8246b19635da61aff5a2f3f339307d6f5eb
Sweet Song <shihui.sweet.song@gmail.com>
2013-07-25 16:20:40 -0400

    deserialize done

--- hive/src/main/java/com/mongodb/hadoop/hive/BSONSerde.java
+++ hive/src/main/java/com/mongodb/hadoop/hive/BSONSerde.java
@@ -1,264 +1,260 @@
 package com.mongodb.hadoop.hive;
 
-/**
- * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
 
-import com.mongodb.hadoop.io.BSONWritable;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.serde.Constants;
-import org.apache.hadoop.hive.serde2.SerDe;
+import org.apache.hadoop.hive.serde.serdeConstants;
+import org.apache.hadoop.hive.serde2.AbstractSerDe;
 import org.apache.hadoop.hive.serde2.SerDeException;
 import org.apache.hadoop.hive.serde2.SerDeStats;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
-import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.typeinfo.ListTypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.MapTypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.StructTypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
 import org.bson.types.ObjectId;
 
-import java.util.*;
+import com.mongodb.hadoop.io.BSONWritable;
 
-public class BSONSerde implements SerDe {
+public class BSONSerde extends AbstractSerDe {
 
+    
+    public boolean DEBUG = true;
     private static final Log LOG = LogFactory.getLog(BSONSerde.class.getName());
 
-    /**
-     * # of columns in the Hive table
-     */
-    private int numColumns;
-    /**
-     * Column names in the Hive table
-     */
+    private StructTypeInfo docTypeInfo;
+    private ObjectInspector docOI;
     private List<String> columnNames;
-    /**
-     * An ObjectInspector which contains metadata
-     * about rows
-     */
-    private StructObjectInspector rowInspect;
-
-    private ArrayList<Object> row;
-
     private List<TypeInfo> columnTypes;
 
-    private long serializedSize;
-    private SerDeStats stats;
-    private boolean lastOperationSerialize;
-    private boolean lastOperationDeserialize;
+    private List<Object> row = new ArrayList<Object>();
 
-    private boolean test = false;
+    /**
+     * Finds out the information of the table, including the column names and types. 
+     */
+    @Override
+    public void initialize(Configuration conf, Properties tblProps)
+            throws SerDeException {
 
-    public void initialize(Configuration sysProps, Properties tblProps)
-                                                throws SerDeException {
-        LOG.debug("Initializing BSONSerde");
+        // Gets the column names
+        String colNamesStr = tblProps.getProperty(serdeConstants.LIST_COLUMNS);
+        columnNames = Arrays.asList(colNamesStr.split(","));
 
-        /*
-         * column names from the Hive table
-         */
-        String colNameProp = tblProps.getProperty(Constants.LIST_COLUMNS);
-        columnNames = Arrays.asList(colNameProp.split(","));
+        // Get the column types
+        String colTypesStr = tblProps.getProperty(serdeConstants.LIST_COLUMN_TYPES);
+        columnTypes = TypeInfoUtils.getTypeInfosFromTypeString(colTypesStr);
 
-        /*
-         * Extract Type Information
-         */
-        String colTypeProp = tblProps.getProperty(Constants.LIST_COLUMN_TYPES);
-        columnTypes = TypeInfoUtils.getTypeInfosFromTypeString(colTypeProp);
+        assert( columnNames.size() == columnTypes.size()) :
+            "Column Names and Types don't match in size";
 
-        assert ( columnNames.size() == columnTypes.size() ) :
-                 "Column Names and Types don't match in size";
-
-        numColumns = columnNames.size();
-
-        /*
-         * Inspect each column
-         */
-        List<ObjectInspector> columnInspects = new ArrayList<ObjectInspector>(
-                                                    columnNames.size() );
-        ObjectInspector inspect;
-        for (int c = 0; c < numColumns; c++) {
-            inspect = TypeInfoUtils.getStandardJavaObjectInspectorFromTypeInfo(
-                columnTypes.get(c) );
-            columnInspects.add( inspect );
+        // Get the structure and object inspector
+        docTypeInfo = 
+                (StructTypeInfo) TypeInfoFactory.getStructTypeInfo(columnNames, columnTypes);
+        docOI = 
+                TypeInfoUtils.getStandardJavaObjectInspectorFromTypeInfo(docTypeInfo);
+        
+        if (DEBUG) {
+            for (int i = 0 ; i < columnTypes.size() ; i++) {
+                /*System.out.println("initilize--" + columnNames.get(i) + ":" 
+            + columnTypes.get(i).getTypeName());*/
+            }
         }
-
-        rowInspect = ObjectInspectorFactory.getStandardStructObjectInspector(
-                        columnNames, columnInspects);
-
-        /*
-         * Create empty row objects which are reused during deser
-         */
-        row = new ArrayList<Object>(numColumns);
-        for (int c = 0; c < numColumns; c++) {
-            row.add(null);
-        }
-
-        stats = new SerDeStats();
-
-
-        LOG.debug("Completed initialization of BSONSerde.");
     }
 
-    public ObjectInspector getObjectInspector() throws SerDeException {
-        return rowInspect;
-    }
+    /**
+     * Given a Writable object of BSON, turn it into a table
+     */
+    @Override
+    public Object deserialize(Writable writ) throws SerDeException {
 
+        BSONObject doc = null;
+        row.clear();
 
-    public Object deserialize(Writable blob) throws SerDeException {
-        LOG.debug("Deserializing BSON Row with Class: " + blob.getClass());
-
-        BSONObject doc;
-
-        if (blob instanceof BSONWritable) {
-            BSONWritable b = (BSONWritable) blob;
-            LOG.debug("Got a BSONWritable: " + b);
-            doc = (BSONObject) b;
+        // Make sure it's a BSONWritable object
+        if (writ instanceof BSONWritable) {
+            doc = ((BSONWritable) writ).getDoc();
         } else {
-            throw new SerDeException(getClass().toString() +
-                    " requires a BSONWritable object, not " + blob.getClass());
+            throw new SerDeException(getClass().toString() + 
+                    " requires a BSONWritable object, not" + writ.getClass());
+        }
+        
+        if (DEBUG) {
+            System.out.println(doc.toString());
         }
 
-        String colName = "";
-        Object value;
-        for (int c = 0; c < numColumns; c++) {
-            try {
-                colName = columnNames.get(c);
-                TypeInfo ti = columnTypes.get(c);
-                String x = "Col #" + c + " Type: " + ti.getTypeName();
-                LOG.trace("***" + x);
-                // Attempt typesafe casting
-                if (!doc.containsField(colName)) {
-                    LOG.debug("Cannot find field '" + colName + "' in " + doc.keySet());
-                    for (String k : doc.keySet()) {
-                        if (k.trim().equalsIgnoreCase(colName)) {
-                            colName = k;
-                            LOG.debug("K: " + k + "colName: " + colName);
-                        } 
-                    }
-                }
-                if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.DOUBLE_TYPE_NAME)) {
-                    value = (Double) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.BIGINT_TYPE_NAME)) {
-                    value = (Long) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.INT_TYPE_NAME)) {
-                    value = doc.get(colName);
-                    /** Some integers end up stored as doubles
-                     * due to quirks of the shell
-                     */
-                    if (value instanceof  Double)
-                        value = ((Double) value).intValue();
-                    else
-                        value = (Integer) value;
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.TINYINT_TYPE_NAME)) {
-                    value = (Byte) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.FLOAT_TYPE_NAME)) {
-                    value = (Float) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                        Constants.BOOLEAN_TYPE_NAME)) {
-                    value = (Boolean) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                                Constants.STRING_TYPE_NAME)) {
-                    value = (String) doc.get(colName);
-                } else if (ti.getTypeName().equalsIgnoreCase(
-                        Constants.DATE_TYPE_NAME) ||
-                        ti.getTypeName().equalsIgnoreCase(
-                                Constants.DATETIME_TYPE_NAME)  ||
-                        ti.getTypeName().equalsIgnoreCase(
-                                Constants.TIMESTAMP_TYPE_NAME)) {
-                    value = (Date) doc.get(colName);
-                } else if (ti.getTypeName().startsWith(Constants.LIST_TYPE_NAME)) {
-                    // Copy to an Object array
-                    BasicBSONList lst = (BasicBSONList) doc.get(colName);
-                    Object[] arr = new Object[lst.size()];
-                    for (int i = 0; i < arr.length; i++) {
-                            arr[i] = lst.get(i);
-                    }
-                    value = arr;
-                } else if (ti.getTypeName().startsWith(Constants.MAP_TYPE_NAME)) {
-                    // Hack in case embedded doc has _id, acting weird
-                    if (doc.containsField("_id")) {
-                        if (doc.get("_id") instanceof ObjectId) {
-                            doc.put("_id", ((ObjectId) doc.get("_id")).toString());
-                        }
-                    }
-                    value = ((BSONObject) doc).toMap();
-                } else if (ti.getTypeName().startsWith(Constants.STRUCT_TYPE_NAME)) {
-                    //value = ((BSONObject) doc).toMap();
-                    throw new IllegalArgumentException("Unable to currently work with structs.");
-                } else {
-                    // Fall back, just get an object
-                    LOG.warn("FALLBACK ON '" + colName + "'");
-                    value = doc.get(colName);
-                }
-                row.set(c, value);
-            } catch (Exception e) {
-                LOG.error("Exception decoding row at column " + colName, e);
+        // Only lower case names
+        BSONObject lower = new BasicBSONObject();
+        for (Entry<String, Object> entry : ((BasicBSONObject) doc).entrySet()) {
+            if (lower.containsField(entry.getKey().toLowerCase())) {
+                LOG.error("Fields should only be lower cased and not duplicated: " 
+                        + entry.getKey());
+            } else {
+                lower.put(entry.getKey().toLowerCase(), entry.getValue());
             }
         }
 
-        LOG.debug("Deserialized Row: " + row);
+        Object value = null;
+        for (String fieldName : docTypeInfo.getAllStructFieldNames()) {
+            try {
+                TypeInfo fieldTypeInfo = docTypeInfo.getStructFieldTypeInfo(fieldName);
+                value = parseField(lower.get(fieldName), fieldTypeInfo);
+                if (DEBUG) {
+                    System.out.println("deserialize--" + fieldName + ":" + value.toString());
+                }
+            } catch (Exception e) {
+                value = null;
+            }
+            row.add(value);
+        }
 
+
+        if (DEBUG) {
+            System.out.println("1 writable done");
+        }
         return row;
     }
 
     /**
-     * Not sure - something to do with serialization of data
+     * For a given Object value and its supposed TypeInfo
+     * determine and return its Java object representation
      */
-    public Class<? extends Writable> getSerializedClass() {
-        return Text.class;
+    private Object parseField(Object value, TypeInfo valueTypeInfo) {
+
+        if (value == null) {
+            return null;
+        }
+        
+        if (DEBUG) {
+            System.out.println("Field-- " +value.toString() + ":" + valueTypeInfo.getCategory().toString());
+        }
+        switch (valueTypeInfo.getCategory()) {
+            case PRIMITIVE:
+                return parsePrimitive(value, valueTypeInfo);
+            case LIST:
+                return parseList(value, (ListTypeInfo) valueTypeInfo);
+            case MAP:
+                return parseMap(value, (MapTypeInfo) valueTypeInfo);
+            case STRUCT:
+                // Mongo has no struct
+                break;
+            case UNION:
+                // Mongo also has no union
+                return null;
+            default:
+                return parseMongoType(value);
+                // Must be an unknown (a Mongo specific type)
+                
+        }
+        return value;
     }
+    
+    private Object parsePrimitive(Object value, TypeInfo valueTypeInfo) {
 
-    public Writable serialize(Object obj, ObjectInspector objInspector)
-                                throws SerDeException {
-        LOG.info("-----------------------------");
-        LOG.info("***** serialize BSON ********");
-        LOG.info("-----------------------------");
-
+        String typeName = valueTypeInfo.getTypeName();        
+        if (DEBUG) {
+            System.out.println("PRIMITIVE-- " + value.getClass().toString() + ":" + typeName);
+        }
+        if (typeName.equalsIgnoreCase(serdeConstants.DOUBLE_TYPE_NAME)) {
+            return (Double) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.BIGINT_TYPE_NAME)) {
+            return (Long) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.INT_TYPE_NAME)) {
+            if (value instanceof Double) {
+                return ((Double) value).intValue();
+            } else if (value instanceof Float) {
+                return ((Float) value).intValue();
+            }
+            return (Integer) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.TINYINT_TYPE_NAME)) {
+            return (Byte) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.FLOAT_TYPE_NAME)) {
+            return (Float) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.BOOLEAN_TYPE_NAME)) {
+            return (Boolean) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.STRING_TYPE_NAME)) {
+            return (String) value;
+        } else if (typeName.equalsIgnoreCase(serdeConstants.DATE_TYPE_NAME)
+                || typeName.equalsIgnoreCase(serdeConstants.DATETIME_TYPE_NAME)
+                || typeName.equalsIgnoreCase(serdeConstants.TIMESTAMP_TYPE_NAME)) {
+            return (Date) value;
+        } else {
+            LOG.equals("not a known primitive type");
+        }
+        return value;
+    }
+    
+    private Object parseList(Object value, ListTypeInfo valueTypeInfo) {
+        BasicBSONList list = (BasicBSONList) value;
+        TypeInfo listTypeInfo = valueTypeInfo.getListElementTypeInfo();
+        if (DEBUG) {
+            System.out.println("in list: " + listTypeInfo.getTypeName());
+        }
+        
+        for (int i = 0 ; i < list.size() ; i++) {
+            list.set(i, parseField(list.get(i), listTypeInfo));
+        }
+        return list.toArray();
+    }
+    
+    private Object parseMap(Object value, MapTypeInfo valueTypeInfo) {
+        BasicBSONObject b = (BasicBSONObject) value;
+        TypeInfo mapTypeInfo = valueTypeInfo.getMapValueTypeInfo();
+        if (DEBUG) {
+            System.out.println("in map: " + mapTypeInfo.getTypeName());
+        }
+        
+        for (Entry<String, Object> entry : b.entrySet()) {
+            b.put(entry.getKey(), parseField(entry.getValue(), mapTypeInfo));
+        }
+        
+        return b.toMap();
+    }
+    
+    /**
+     * 
+     * For Mongo Specific types, return the most appropriate java types
+     */
+    private Object parseMongoType(Object value) {
+        if (value instanceof ObjectId) {
+            return value.toString();
+        }
         return null;
     }
 
+    @Override
+    public ObjectInspector getObjectInspector() throws SerDeException {
+        return docOI;
+    }
+
+    @Override
     public SerDeStats getSerDeStats() {
-        stats.setRawDataSize(serializedSize);
-        return stats;
+        //TODO:: this needs to be determined what it is.
+        return null;
     }
 
-
-
-    private static final byte[] HEX_CHAR = new byte[] {
-            '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' ,
-            '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F'
-    };
-
-    protected static void dumpBytes( byte[] buffer ){
-        StringBuilder sb = new StringBuilder( 2 + ( 3 * buffer.length ) );
-
-        for ( byte b : buffer ){
-            sb.append( "0x" ).append( (char) ( HEX_CHAR[( b & 0x00F0 ) >> 4] ) ).append(
-                    (char) ( HEX_CHAR[b & 0x000F] ) ).append( " " );
-        }
-
-        LOG.info( "Byte Dump: " + sb.toString() );
+    @Override
+    public Class<? extends Writable> getSerializedClass() {
+        return BSONWritable.class;
     }
+
+    @Override
+    public Writable serialize(Object arg0, ObjectInspector arg1)
+            throws SerDeException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
