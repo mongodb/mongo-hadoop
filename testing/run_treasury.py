112876345cf8d7fa63b12c9ade37bb8d64ad83b8
Phillip Quiza <pilliq@mongodb.com>
2013-11-18 15:23:17 -0500

    Bump version for tests

--- testing/run_treasury.py
+++ testing/run_treasury.py
@@ -35,7 +35,7 @@ if not os.path.isdir(TEMPDIR):
 
 #declare -a job_args
 #cd ..
-VERSION_SUFFIX = "1.1.0"
+VERSION_SUFFIX = "1.2.0-rc0"
 
 
 def generate_id(size=6, chars=string.ascii_uppercase + string.digits):
