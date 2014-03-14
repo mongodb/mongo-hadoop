d961b679b2a7770c8dd3362e0069b094fdbbd3ce
Justin Lee <justin.lee@10gen.com>
2014-03-14 17:39:37 -0400

    add support for hadoop 2.3 automatically install the version of hadoop specified by -Phadoop_version and run the samples using that install

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for version in "0.23" "1.0" "1.1" "cdh4" "2.2"
+for version in "0.23" "1.0" "1.1" "2.2" "cdh4" "2.3"
 do
-	./gradlew -Phadoop_version=${version} clean $*
+./gradlew -Phadoop_version=${version} clean $* # 2>&1 | tee gradle-${version}.out
 done
