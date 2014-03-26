a066d25b3b55f34d199d97620984163a765ccc49
Justin Lee <justin.lee@10gen.com>
2014-03-26 16:37:35 -0400

    pass along version information remove debug

--- testing/src/test/java/org/mongodb/hadoop/BaseHadoopTest.java
+++ testing/src/test/java/org/mongodb/hadoop/BaseHadoopTest.java
@@ -37,7 +37,7 @@ public class BaseHadoopTest {
     private static final Log LOG = LogFactory.getLog(BaseHadoopTest.class);
 
     public static String HADOOP_HOME;
-    public static String HADOOP_VERSION = loadProperty("hadoop_version", "2.3");
+    public static String HADOOP_VERSION = loadProperty("HADOOP_VERSION", "2.3");
 
     public static final File JSONFILE_PATH = new File("../examples/treasury_yield/src/main/resources/yield_historical_in.json");
     protected static final File JOBJAR_PATH = new File("../examples/treasury_yield/build/libs").listFiles(new HadoopVersionFilter())[0];
@@ -279,11 +279,7 @@ public class BaseHadoopTest {
     private static class HadoopVersionFilter implements FileFilter {
         @Override
         public boolean accept(final File pathname) {
-            System.out.println("HADOOP_VERSION = " + HADOOP_VERSION);
-            System.out.println("pathname = " + pathname);
-            boolean accept = pathname.getName().endsWith(format("_%s.jar", HADOOP_VERSION));
-            System.out.println("accept = " + accept);
-            return accept;
+            return pathname.getName().endsWith(format("_%s.jar", HADOOP_VERSION));
         }
     }
 }
