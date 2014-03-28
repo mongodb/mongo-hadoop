dde564d7dfaf5a98c5bbbfed6a3a43fe4e6414a0
Jeremy Karn <jkarn@mortardata.com>
2014-03-27 21:33:05 -0400

    Add tests that run full Pig scripts against local Mongo database.

--- pig/src/test/java/com/mongodb/hadoop/pig/MongoLoaderTest.java
+++ pig/src/test/java/com/mongodb/hadoop/pig/MongoLoaderTest.java
@@ -1,312 +1,165 @@
 package com.mongodb.hadoop.pig;
 
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.apache.pig.data.DataBag;
-import org.apache.pig.data.Tuple;
-import org.junit.Test;
-
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.Tuple;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 
 @SuppressWarnings("rawtypes")
 public class MongoLoaderTest {
-    @Test
-    public void testSimpleChararray() throws IOException {
-        String userSchema = "d:chararray";
-        MongoLoader ml = new MongoLoader(userSchema);
+    private static PigServer pigServerLocal = null;
+    private static String dbName = "MongoLoaderTest-" + new Date().getTime();
 
-        Object result = BSONLoader.readField("value", ml.getFields()[0]);
-        assertEquals("value", result);
+    @AfterClass
+    public static void tearDown() throws Exception {
+        new MongoClient().dropDatabase(dbName);
+    }
+    
+    private void insertData(String collectionName, BasicDBObject... objs) throws Exception {
+        MongoClient mc = new MongoClient();
+        DBCollection col = mc.getDB(dbName).getCollection(collectionName);
+        col.insert(objs);
+    }
+
+    @Test
+    public void testSimpleChararray_fullRun() throws Exception {
+        BasicDBObject obj1 = new BasicDBObject("a", "value1");
+        BasicDBObject obj2 = new BasicDBObject("a", "value2");
+        insertData("testSimpleChararray", obj1, obj2);
+        
+        pigServerLocal = new PigServer(ExecType.LOCAL);
+        
+        //Test no schema
+        pigServerLocal.registerQuery(String.format(
+                "A = LOAD 'mongodb://localhost:27017/%s.%s' using com.mongodb.hadoop.pig.MongoLoader();", dbName, "testSimpleChararray"));
+
+        Iterator<Tuple> iter = pigServerLocal.openIterator("A");
+        assertTrue(iter.hasNext());
+        Tuple result1 = iter.next();
+        assertTrue(iter.hasNext());
+        Tuple result2 = iter.next();
+        assertFalse(iter.hasNext());
+        
+        Map map1 = (Map) result1.get(0);
+        assertEquals(obj1.getString("a"), map1.get("a"));
+        assertEquals(obj1.getObjectId("_id"), map1.get("_id"));
+
+        Map map2 = (Map) result2.get(0);
+        assertEquals(obj2.getString("a"), map2.get("a"));
+        assertEquals(obj2.getObjectId("_id"), map2.get("_id"));
+        
+        
+        //Test Schema and idAlias
+        pigServerLocal.registerQuery(String.format(
+                "B = LOAD 'mongodb://localhost:27017/%s.%s' using com.mongodb.hadoop.pig.MongoLoader('mongo_id:chararray, a:chararray', 'mongo_id');", dbName, "testSimpleChararray"));
+        
+        iter = pigServerLocal.openIterator("B");
+        assertTrue(iter.hasNext());
+        result1 = iter.next();
+        assertTrue(iter.hasNext());
+        result2 = iter.next();
+        assertFalse(iter.hasNext());
+        
+        assertEquals(obj1.getObjectId("_id"), result1.get(0));
+        assertEquals(obj1.getString("a"), result1.get(1));
+        assertEquals(obj2.getObjectId("_id"), result2.get(0));
+        assertEquals(obj2.getString("a"), result2.get(1));
+        
+        
+        //Test loadaschararray
+        pigServerLocal.registerQuery(String.format(
+                "C = LOAD 'mongodb://localhost:27017/%s.%s' using com.mongodb.hadoop.pig.MongoLoader('s:chararray', '', '--loadaschararray');", dbName, "testSimpleChararray"));
+        
+        iter = pigServerLocal.openIterator("C");
+        assertTrue(iter.hasNext());
+        result1 = iter.next();
+        assertTrue(iter.hasNext());
+        result2 = iter.next();
+        assertFalse(iter.hasNext());
+        
+        assertEquals( String.format("{ \"_id\" : { \"$oid\" : \"%s\"} , \"a\" : \"%s\"}", obj1.getObjectId("_id"), obj1.getString("a"))
+                    , result1.get(0));
+        assertEquals( String.format("{ \"_id\" : { \"$oid\" : \"%s\"} , \"a\" : \"%s\"}", obj2.getObjectId("_id"), obj2.getString("a"))
+                    , result2.get(0));
     }
     
     @Test
-    public void testSimpleFloat() throws IOException {
-        String userSchema = "d:float";
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(1.1F, ml.getFields()[0]);
-        assertEquals(1.1F, result);
-    }
-    
-    @Test
-    public void testSimpleFloatAsDouble() throws IOException {
-        String userSchema = "d:float";
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(1.1D, ml.getFields()[0]);
-        assertEquals(1.1F, result);
-    }
-    
-    @Test
-    public void testSimpleTuple() throws IOException {
-        String userSchema = "t:tuple(t1:chararray, t2:chararray)";
-        Object val = new BasicDBObject()
-            .append("t1", "t1_value")
-            .append("t2", "t2_value");
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(val, ml.getFields()[0]);
-        
-        Tuple t = (Tuple) result;
-        assertEquals(2, t.size());
-        assertEquals("t1_value", t.get(0));
-        assertEquals("t2_value", t.get(1));
-    }
-    
-    @Test
-    public void testSimpleTupleMissingField() throws IOException {
-        String userSchema = "t:tuple(t1:chararray, t2:chararray, t3:chararray)";
-        Object val = new BasicDBObject()
-            .append("t1", "t1_value")
-            .append("t2", "t2_value");
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(val, ml.getFields()[0]);
-        
-        Tuple t = (Tuple) result;
-        assertEquals(3, t.size());
-        assertEquals("t1_value", t.get(0));
-        assertEquals("t2_value", t.get(1));
-        assertNull(t.get(2));
-    }
-    
-    @Test
-    public void testSimpleTupleIncorrectFieldType() throws IOException {
-        String userSchema = "t:tuple(t1:chararray, t2:float)";
-        Object val = new BasicDBObject()
-            .append("t1", "t1_value")
-            .append("t2", "t2_value");
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(val, ml.getFields()[0]);
-        
-        Tuple t = (Tuple) result;
-        assertEquals(2, t.size());
-        assertEquals("t1_value", t.get(0));
-        assertNull(t.get(1));
-    }
-    
-    @Test
-    public void testSimpleBag() throws IOException {
-        String userSchema = "b:{t:tuple(t1:chararray, t2:chararray)}";
-        BasicDBList bag = new BasicDBList();
-        bag.add(new BasicDBObject()
-                        .append("t1", "t11_value")
-                        .append("t2", "t12_value"));
-        bag.add(new BasicDBObject()
-                        .append("t1", "t21_value")
-                        .append("t2", "t22_value"));
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(bag, ml.getFields()[0]);
-        
-        DataBag b = (DataBag) result;
-        Iterator<Tuple> bit = b.iterator();
-        
-        Tuple firstInnerT = bit.next();
-        assertEquals(2, firstInnerT.size());
-        assertEquals("t11_value", firstInnerT.get(0));
-        assertEquals("t12_value", firstInnerT.get(1));
-        
-        Tuple secondInnerT = bit.next();
-        assertEquals(2, secondInnerT.size());
-        assertEquals("t21_value", secondInnerT.get(0));
-        assertEquals("t22_value", secondInnerT.get(1));
-        
-        assertFalse(bit.hasNext());
-    }
-    
-    @Test
-    public void testBagThatIsNotABag() throws IOException {
-        String userSchema = "b:{t:tuple(t1:chararray, t2:chararray)}";
-        BasicDBObject notABag = new BasicDBObject();
-        notABag.append("f1", new BasicDBObject()
-                        .append("t1", "t11_value")
-                        .append("t2", "t12_value"));
-        notABag.append("f2", new BasicDBObject()
-                        .append("t1", "t21_value")
-                        .append("t2", "t22_value"));
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        Object result = BSONLoader.readField(notABag, ml.getFields()[0]);
-        assertNull(result);
-    }
-    
-    @Test
-    public void testDeepness() throws IOException {
-        String userSchema = "b:{t:tuple(t1:chararray, b:{t:tuple(i1:int, i2:int)})}";
-        
+    public void testEmbeddedObject_fullRun() throws Exception {
+        // Input Data: {"f1":"v1", "f2":2, "f3": [1,2,3], "f4":{"i1":"inner1","i2":"inner2"}}
         BasicDBList innerBag = new BasicDBList();
-        innerBag.add(new BasicDBObject()
-                        .append("i1", 1)
-                        .append("i2", 2));
-        innerBag.add(new BasicDBObject()
-                        .append("i1", 3)
-                        .append("i2", 4));
-
-        BasicDBList bag = new BasicDBList();
-        bag.add(new BasicDBObject()
-                    .append("t1", "t1_value")
-                    .append("b", innerBag));
-
-        MongoLoader ml = new MongoLoader(userSchema);
-
-        DataBag result = (DataBag) BSONLoader.readField(bag, ml.getFields()[0]);
-        assertEquals(1, result.size());
-        
-        Iterator<Tuple> bit = result.iterator();
-        Tuple t = bit.next();
-        
-        assertEquals(2, t.size());
-        
-        DataBag innerBagResult = (DataBag) t.get(1);
-        assertEquals(2, innerBagResult.size());
-        
-        Iterator<Tuple> innerBit = innerBagResult.iterator();
-        Tuple innerT = innerBit.next();
-        
-        assertEquals(2, innerT.get(1));
-    }
-    
-    @Test
-    public void testSimpleMap() throws Exception {
-        //String userSchema = "m:[int]";
-        // Note: before pig 0.9, explicitly setting the type for
-        // map keys was not allowed, so can't test that here :(
-        String userSchema = "m:[]";
+        innerBag.addAll(Arrays.asList(1,2,3));
+        BasicDBObject innerObj = new BasicDBObject()
+                        .append("i1", "inner1")
+                        .append("i2", 2);
         BasicDBObject obj = new BasicDBObject()
-            .append("k1", 1)
-            .append("k2", 2);
+            .append("f1", "v1")
+            .append("f2",  2)
+            .append("f3", innerBag)
+            .append("f4", innerObj)
+        ;
+        insertData("testEmbeddedObject", obj);
         
-        MongoLoader ml = new MongoLoader(userSchema);
-        Map m = (Map) BSONLoader.readField(obj, ml.getFields()[0]);
+        pigServerLocal = new PigServer(ExecType.LOCAL);
+        
+        //Test no schema
+        pigServerLocal.registerQuery(String.format(
+                "A = LOAD 'mongodb://localhost:27017/%s.%s' using com.mongodb.hadoop.pig.MongoLoader();", dbName, "testEmbeddedObject"));
 
-        assertEquals(2, m.size());
-        assertEquals(1, m.get("k1"));
-        assertEquals(2, m.get("k2"));
-    }
-    
-    @Test
-    public void testMapWithTuple() throws Exception {
-        //String userSchema = "m:[(t1:chararray, t2:int)]";
-        // Note: before pig 0.9, explicitly setting the type for
-        // map keys was not allowed, so can't test that here :(
-        String userSchema = "m:[]";
-        BasicDBObject v1 = new BasicDBObject()
-            .append("t1", "t11 value")
-            .append("t2", 12);
-        BasicDBObject v2 = new BasicDBObject()
-            .append("t1", "t21 value")
-            .append("t2", 22);
-        BasicDBObject obj = new BasicDBObject()
-            .append("v1", v1)
-            .append("v2", v2);
+        Iterator<Tuple> iter = pigServerLocal.openIterator("A");
+        assertTrue(iter.hasNext());
+        Tuple result1 = iter.next();
+        assertFalse(iter.hasNext());
         
-        MongoLoader ml = new MongoLoader(userSchema);
-        Map m = (Map) BSONLoader.readField(obj, ml.getFields()[0]);
-
-        assertEquals(2, m.size());
+        Map map1 = (Map) result1.get(0);
+        assertEquals(obj.getString("_id"), map1.get("_id"));
+        assertEquals(obj.getString("f1"), map1.get("f1"));
+        assertEquals(obj.getInt("f2"), map1.get("f2"));
         
-        /* We can't safely cast to Tuple here 
-         * because pig < 0.9 doesn't allow setting types.
-         * Skip for now.
-
-        Tuple t1 = (Tuple) m.get("v1");
-        assertEquals("t11 value", t1.get(0));
-        assertEquals(12, t1.get(1));
+        DataBag bag = (DataBag) map1.get("f3");
+        assertEquals(innerBag.size(), bag.size());
+        assertEquals(1, bag.iterator().next().get(0));
         
-        Tuple t2 = (Tuple) m.get("v2");
-        assertEquals("t21 value", t2.get(0));
-        */
-    }
-    
-    @Test
-    public void mapWithMap_noSchema() throws Exception {
-        BasicDBObject v1 = new BasicDBObject()
-            .append("t1", "t11 value")
-            .append("t2", 12);
-        BasicDBObject v2 = new BasicDBObject()
-            .append("t1", "t21 value")
-            .append("t2", 22);
-        BasicDBObject obj = new BasicDBObject()
-            .append("v1", v1)
-            .append("v2", v2);
+        Map innerMap = (Map) map1.get("f4");
+        assertEquals(innerObj.get("i1"), innerMap.get("i1"));
+        assertEquals(innerObj.get("i2"), innerMap.get("i2"));
         
-        Map m = (Map) BSONLoader.convertBSONtoPigType(obj);
-
-        assertEquals(2, m.size());
         
-        Map m1 = (Map) m.get("v1");
-        assertEquals("t11 value", m1.get("t1"));
-        assertEquals(12, m1.get("t2"));
+        //Test Schema and idAlias
+        pigServerLocal.registerQuery(String.format(
+                "B = LOAD 'mongodb://localhost:27017/%s.%s' using com.mongodb.hadoop.pig.MongoLoader('mongo_id:chararray, f1:chararray, f2:int, f3:bag{}, f4:(i1:chararray, i2:int)', 'mongo_id');", dbName, "testEmbeddedObject"));
         
-        Map m2 = (Map) m.get("v2");
-        assertEquals("t21 value", m2.get("t1"));
-    }
-    
-    @Test
-    public void mapWithList() throws Exception {
-        BasicDBObject v1 = new BasicDBObject()
-            .append("t1", "t1 value")
-            .append("t2", 12);
-        BasicDBObject v2 = new BasicDBObject()
-            .append("t1", "t1 value")
-            .append("t2", 22);
-        BasicDBList vl = new BasicDBList();
-        vl.add(v1);
-        vl.add(v2);
+        iter = pigServerLocal.openIterator("B");
+        assertTrue(iter.hasNext());
+        result1 = iter.next();
+        assertFalse(iter.hasNext());
         
-        BasicDBObject obj = new BasicDBObject()
-            .append("some_list", vl);
-
-
-        Map m = (Map) BSONLoader.convertBSONtoPigType(obj);
-        assertEquals(1, m.size());
+        assertEquals(obj.getObjectId("_id"), result1.get(0));
+        assertEquals(obj.getString("f1"), result1.get(1));
+        assertEquals(obj.get("f2"), result1.get(2));
         
-        DataBag bag = (DataBag) m.get("some_list");
-        assertEquals(2, bag.size());
+        bag = (DataBag) result1.get(3);
+        assertEquals(innerBag.size(), bag.size());
+        assertEquals(1, bag.iterator().next().get(0));
         
-        Iterator<Tuple> bit = bag.iterator();
-        Tuple t = bit.next();
-        
-        assertEquals(1, t.size());
-        
-        Map innerMap = (Map) t.get(0);
-        assertEquals("t1 value", innerMap.get("t1"));
-    }
-    
-    @Test
-    public void testReadField_mapWithSimpleList_noSchema() throws Exception {
-        BasicDBList vl = new BasicDBList();
-        vl.add("v1");
-        vl.add("v2");
-        
-        BasicDBObject obj = new BasicDBObject()
-            .append("some_list", vl);
-
-        Map m = (Map) BSONLoader.convertBSONtoPigType(obj);
-
-        assertEquals(1, m.size());
-        
-        DataBag bag = (DataBag) m.get("some_list");
-        assertEquals(2, bag.size());
-        
-        Iterator<Tuple> bit = bag.iterator();
-        Tuple t = bit.next();
-        
-        assertEquals(1, t.size());
-        assertEquals("v1", t.get(0));
-        
-        t = bit.next();
-        assertEquals(1, t.size());
-        assertEquals("v2", t.get(0));
+        Tuple innerTuple = (Tuple) result1.get(4);
+        assertEquals(innerObj.get("i1"), innerTuple.get(0));
+        assertEquals(innerObj.get("i2"), innerTuple.get(1));
     }
     
     
---
+++ pig/src/test/java/com/mongodb/hadoop/pig/MongoUpdateStorageTest.java
@@ -0,0 +1,76 @@
+package com.mongodb.hadoop.pig;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.test.Util;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+public class MongoUpdateStorageTest {
+    private static PigServer pigServerLocal = null;
+    private static String dbName = "MongoUpdateStorageTest-" + new Date().getTime();
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        new MongoClient().dropDatabase(dbName);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        BasicDBObject obj1 = new BasicDBObject()
+                                   .append("f1", "a")
+                                   .append("f2", "value1");
+        BasicDBObject obj2 = new BasicDBObject()
+                                   .append("f1", "b")
+                                   .append("f2", "value2");
+        insertData("testUpdate", obj1, obj2);
+
+        String[] input = {
+            "a\tnewValue1\t1",
+            "b\tnewValue2\t2"
+        };
+        Util.createLocalInputFile("simple_input", input);
+        
+        pigServerLocal = new PigServer(ExecType.LOCAL);
+        pigServerLocal.registerQuery("A = LOAD 'simple_input' as (f1:chararray, f2:chararray, f3:int);");
+        pigServerLocal.registerQuery(String.format(
+                "STORE A INTO 'mongodb://localhost:27017/%s.%s' USING com.mongodb.hadoop.pig.MongoUpdateStorage("
+              + "  '{f1:\"\\\\$f1\"}',"
+              + "  '{\\\\$set:{f2:\"\\\\$f2\", f3:\"\\\\$f3\"}}',"
+              + "  'f1:chararray, f2:chararray, f3:int'"
+              + ");", dbName, "update_simple"));
+        pigServerLocal.setBatchOn();
+        pigServerLocal.executeBatch();
+        
+        MongoClient mc = new MongoClient();
+        DBCollection col = mc.getDB(dbName).getCollection("update_simple");
+        
+        DBCursor cursor = col.find();
+        
+        assertEquals(2, cursor.size());
+        DBObject result1 = cursor.next();
+        assertEquals("a", result1.get("f1"));
+        assertEquals("newValue1", result1.get("f2"));
+        assertEquals(1, result1.get("f3"));
+        DBObject result2 = cursor.next();
+        assertEquals("b", result2.get("f1"));
+        assertEquals("newValue2", result2.get("f2"));
+        assertEquals(2, result2.get("f3"));
+    }
+    
+    private void insertData(String collectionName, BasicDBObject... objs) throws Exception {
+        MongoClient mc = new MongoClient();
+        DBCollection col = mc.getDB(dbName).getCollection(collectionName);
+        col.insert(objs);
+    }
+}
