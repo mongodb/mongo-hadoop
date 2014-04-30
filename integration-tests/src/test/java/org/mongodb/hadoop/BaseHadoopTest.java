c626abfa9f42845b5cb6d9df2df553b0702ce8b2
Justin Lee <justin.lee@10gen.com>
2014-04-30 15:41:33 -0400

    checkstyle.  what else?

--- integration-tests/src/test/java/org/mongodb/hadoop/BaseHadoopTest.java
+++ integration-tests/src/test/java/org/mongodb/hadoop/BaseHadoopTest.java
@@ -138,9 +138,9 @@ public class BaseHadoopTest {
             if (isAuthEnabled()) {
                 List<String> list = new ArrayList<String>(asList("-u", "bob",
                                                                  "-p", "pwd123"));
-                String mongodb_server = System.getProperty("mongodb_server", "");
-                LOG.info(format("************ mongodb_server = %s", mongodb_server));
-                if (!mongodb_server.equals("22-release")) {
+                String version = System.getProperty("mongodb_server", "");
+                LOG.info(format("************ version = %s", version));
+                if (!version.equals("22-release")) {
                     list.addAll(asList("--authenticationDatabase", "admin"));
                 }
                 command.addAll(list);
