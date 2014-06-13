0eb43145d0564474511707069c69570f30f769af
Justin Lee <jlee@antwerkz.com>
2014-06-13 11:55:20 -0400

    add cdh5 and 2.4

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for version in "0.23" "1.0" "1.1" "2.2" "cdh4" "2.3"
+for version in "0.23" "1.0" "1.1" "2.2" "cdh4" "cdh5" "2.3" "2.4"
 do
 ./gradlew -Phadoop_version=${version} clean $* # 2>&1 | tee gradle-${version}.out
 done
