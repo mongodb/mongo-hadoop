ca1ff5c3a01e58fa25c26ad733b5d56e514a8520
Cristi Burca <mail@scribu.net>
2018-06-11 17:07:22 +0100

    Use the binary stdin and stdout streams

--- streaming/language_support/python/pymongo_hadoop/input.py
+++ streaming/language_support/python/pymongo_hadoop/input.py
@@ -14,7 +14,7 @@ class BSONInput(object):
     https://github.com/klbostee/typedbytes
     """
 
-    def __init__(self, fh=sys.stdin, unicode_errors='strict'):
+    def __init__(self, fh=sys.stdin.buffer, unicode_errors='strict'):
         self.fh = fh
         self.unicode_errors = unicode_errors
         self.eof = False
